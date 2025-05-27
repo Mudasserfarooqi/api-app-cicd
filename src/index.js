d61b831c436c56fe957ff8b5801c9bb6ad4488f0
mudasserfarooqi <mudasserfarooqi77@gmail.com>
2025-05-27 18:15:13 +0500

    Next Update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// src/index.js
+\// src/index.js
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,7 +8,7 @@ app.use(express.json());
 
 // Basic route
 app.get('/', (req, res) => {
-  res.send('Hello Everyone. Welcome to DevOps CICD Pipeline through Github Actions - Task Manager API is running!');
+  res.send('Hello Everyone. Welcome to DevOps CICD Pipeline through Github Actions - Its Task Manager API is running application!');
 });
 
 // Start the server
